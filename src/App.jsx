d6db69ff25d9ef21af625632731972482abf95d8
szatgabor <gabor.samers@gmail.com>
2021-04-12 12:24:57 +0200

    created react structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import QuoteContainer from './QuoteContainer/QuoteContainer'
+
+function App() {
+    return (
+        <QuoteContainer />
+    )
+}
+
+export default App
---
+++ src/QuoteContainer/QuoteContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './QuoteContainer.css'
+
+function QuoteContainer() {
+    return (
+        <div className="quote-container">
+            <h1>Text</h1>
+        </div>
+    )
+}
+
+export default QuoteContainer
